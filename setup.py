43c6f95d6e9b1d5cf549bf1bc20a58b908c658bb
Mike Alexander <mikealexander1860@gmail.com>
2018-06-13 17:18:00 -0700

    updated setup.py and package __init__ to support PyPi upload

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+# To upload to PyPi, find the directions here https://packaging.python.org/tutorials/packaging-projects/
+
 
 import sys
 from setuptools import setup, find_packages
@@ -12,19 +14,46 @@ if (sys.version_info < (3,0)):
 else:
     install_requires.append('python-ntlm3')
 
+# get our long description from the README.md
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
   name = "v1pysdk",
-  version = "0.5",
+  version = "0.5.0",
   description = "VersionOne API client",
   author = "Joe Koberg (VersionOne, Inc.)",
   author_email = "Joe.Koberg@versionone.com",
+  long_description = long_description,
+  long_description_content_type = "text/markdown",
   license = "MIT/BSD",
   keywords = "versionone v1 api sdk",
   url = "http://github.com/mtalexan/VersionOne.SDK.Python.git",
+  project_urls={
+      'Documentation': 'http://github.com/mtalexan/VersionOne.SDK.Python.git',
+      'Source' : 'http://github.com/mtalexan/VersionOne.SDK.Python.git',
+      'Tracker' : 'http://github.com/mtalexan/VersionOne.SDK.Python.git/issues',
+      },
 
   packages = [
     'v1pysdk',
     ],
+
+  classifiers=(
+      "Development Status :: 4 - Beta",
+      "Intended Audience :: Developers",
+      "License :: OSI Approved :: MIT License",
+      "License :: OSI Approved :: BSD License",
+      "Operating System :: OS Independent",
+      "Programming Language :: Python :: 2",
+      "Programming Language :: Python :: 2.7",
+      "Programming Language :: Python :: 3",
+      "Programming Language :: Python :: 3.5",
+      "Topic :: Software Development :: Bug Tracking",
+      ),
+
+  # it may work on others, but this is what has had basic testing
+  python_requires='>=2.5, <4',
 
   install_requires = install_requires,
 
