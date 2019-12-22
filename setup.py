f579fdf38c53b83e2f24def3424cc925ea2bb374
Mike Alexander <mikealexander1860@gmail.com>
2019-12-21 17:15:40 -0800

    Update for newer python version

--- setup.py
+++ setup.py
@@ -51,6 +51,9 @@ setup(
       "Programming Language :: Python :: 2.7",
       "Programming Language :: Python :: 3",
       "Programming Language :: Python :: 3.5",
+      "Programming Language :: Python :: 3.6",
+      "Programming Language :: Python :: 3.7",
+      "Programming Language :: Python :: 3.8",
       "Topic :: Software Development :: Bug Tracking",
       ),
 
